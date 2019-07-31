420bc6fdc7d6b08f9ab7a214fa462de5fc089ab9
Lily <phuonglinh83@gmail.com>
2019-07-31 13:19:57 -0700

    Update Heroku db init script

--- bin/initHerokuDB.sh
+++ bin/initHerokuDB.sh
@@ -1,7 +1,5 @@
 heroku addons:create heroku-postgresql:hobby-dev
-heroku run sequelize db:drop
 heroku run sequelize db:migrate
-heroku run sequelize db:create
 heroku pg:psql < db/load/load_users.sql
 heroku pg:psql < db/load/load_videos.sql
 heroku pg:psql < db/load/load_categories.sql
